04d13c5f1e06deaad8e5f767d0afddf31b90da6a
oluwatobi-fasasi <wasiufasasi79@gmail.com>
2024-03-08 23:04:24 -0500

    Added my project

---
+++ app/dashboard/invoice/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function InvoicePage() {
+  return <div>InvoicePage</div>;
+}
+
+export default InvoicePage;
---
+++ app/ui/font.ts
@@ -0,0 +1,4 @@
+import { Inter, Lusitana } from 'next/font/google';
+
+export const myInter = Inter({ subsets: ['latin'] });
+export const myLuci = Lusitana({ weight: ['400', '700'], subsets: ['latin'] });
